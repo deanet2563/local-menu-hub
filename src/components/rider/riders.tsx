2d7cd498ad0fd42543ded440a8b758a675819d1e
deanet2563 <deane.t2563@gmail.com>
2026-08-01 14:22:01 +0700

    Delete src/components/rider/riders.tsx

--- src/components/rider/riders.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { RiderVerificationPanel } from "@/components/admin/RiderVerificationPanel";
-
-export const Route = createFileRoute("/sweet/riders")({
-  component: RiderVerificationPanel,
-});
