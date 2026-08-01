1b18822bd70da14f917a09f895e084cb99655dbc
deanet2563 <deane.t2563@gmail.com>
2026-08-01 14:21:39 +0700

    Delete src/components/rider/signup.tsx

--- src/components/rider/signup.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { RiderSignupForm } from "@/components/rider/RiderSignupForm";
-
-export const Route = createFileRoute("/rider/signup")({
-  component: RiderSignupForm,
-});
